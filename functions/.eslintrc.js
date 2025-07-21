24b916dcca326af47bcba80d9bf1aa26c550dc7d
Lokesh <lokeshnagarajan151105@gmail.com>
2025-07-21 23:04:58 +0530

    Prepare backend for Render deployment

---
+++ functions/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "no-unused-vars": ["error", {"argsIgnorePattern": "^_"}],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {
+    "global": true,
+    "module": true,
+    "require": true,
+    "console": true,
+  },
+};
